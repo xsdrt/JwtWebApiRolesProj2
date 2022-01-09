e65185c4f4c65403e593f1104cc660b1c892c068
xsdrt <m_redinger@hotmail.com>
2022-01-08 21:21:52 -0500

    Added the Authorize attribute to the WeatherForecastContoller HttpGet call so only authorized users can get the forecast.  Als added the Authenticatin scheme, and middleware to the program.cs

--- JwtWebApiRolesProj2/Controllers/WeatherForecastController.cs
+++ JwtWebApiRolesProj2/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JwtWebApiRolesProj2.Controllers
@@ -18,7 +19,7 @@ namespace JwtWebApiRolesProj2.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherForecast"), Authorize]  //Decorate the HttpGet with the Authorize attribute and reference the using Microsoft.AspNetCore.Authorization... 
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
--- JwtWebApiRolesProj2/Program.cs
+++ JwtWebApiRolesProj2/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)         //This is the Authentication Scheme
+    .AddJwtBearer(options => {
+        options.TokenValidationParameters = new TokenValidationParameters       //Make sure and add reference using Microsoft.IdentityModel.Tokens...
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+
+        };
+    });
 
 var app = builder.Build();
 
@@ -17,6 +33,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();        // Add this middleware. Also make sure this is above the app.UseAuthorization();
 
 app.UseAuthorization();
 
